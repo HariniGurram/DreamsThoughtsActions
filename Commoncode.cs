70dfa99e2124b9b14d22a5e2c6ba5183366d2760
HariniGurram <83755246+HariniGurram@users.noreply.github.com>
2021-05-06 15:47:55 +0530

    this is for testing

---
+++ Commoncode.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Chrome;
+
+
+namespace AutomationLibraries
+{
+    
+    public static class Commoncode
+    {
+        public static IWebDriver driver;
+        public static void clickbutton(string logicalname)
+        {
+
+        }
+
+        public static void selectdropdown(string value ,string logicalname)
+        {
+
+        }
+
+        public static void entertext(string logicalname)
+        {
+
+        }
+
+        public static void driversetup()
+        {
+            string path = Environment.GetEnvironmentVariable("chromepath");
+            driver = new ChromeDriver(path);
+            driver.Navigate().GoToUrl("http://demo.automationtesting.in/");
+            driver.Manage().Window.Maximize();
+
+        }
+
+        public static void openurl()
+        {
+            try
+            {
+
+
+            }
+            catch
+            {
+
+            }
+
+
+        }
+
+
+        public static void closedriver()
+        {
+            driver.Close();
+
+        }
+
+    }
+}
