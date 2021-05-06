9b2c66b97c38a8defeba76e73575665cdd72194f
HariniGurram <gurramharini1991@gmail.com>
2021-05-06 16:35:09 +0530

    Committing entertext method

--- Commoncode.cs
+++ Commoncode.cs
@@ -16,7 +16,7 @@ namespace AutomationLibraries
         public static IWebDriver driver;
         public static void clickbutton(string logicalname)
         {
-                driver.Click();
+                ele.Click();
         }
 
         public static void selectdropdown(string value ,string logicalname)
@@ -26,7 +26,7 @@ namespace AutomationLibraries
 
         public static void entertext(string logicalname)
         {
-
+            ele.sendkeys();
         }
 
         public static void driversetup()
